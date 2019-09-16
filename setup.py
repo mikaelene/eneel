e1a470519a119616f6f5c2b19c65665188e60ced
Mikael Ene <mikael.ene@gmail.com>
2019-09-16 16:11:18 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/scripts/env python
+from setuptools import find_packages
+from distutils.core import setup
+
+package_name = "eneel"
+package_version = "0.1.0"
+description = """A package for fast loading av relation data"""
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author="Mikael Ene",
+    author_email="mikael.ene@gmail.com",
+    url="https://github.com/mikaelene/eneel",
+    packages=find_packages(),
+    install_requires=[
+        'pyodbc>=4.0.27',
+        'psycopg2>=2.7.7',
+        'PyYAML>=3.11',
+    ],
+    entry_points={
+            'console_scripts': ['eneel=eneel.main:main'],
+        },
+)
