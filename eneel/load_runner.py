5408de375f3bf1f60cb57adcec428e76a218ff5d
Mikael Ene <mikael.ene@gmail.com>
2019-09-16 22:30:27 +0200

    added multiprocessing of loads

--- eneel/load_runner.py
+++ eneel/load_runner.py
@@ -1,4 +1,5 @@
 import eneel.utils as utils
+from concurrent.futures import ProcessPoolExecutor as Executor, as_completed
 
 
 def run_project(connections_path, project_path):
@@ -7,34 +8,51 @@ def run_project(connections_path, project_path):
 
     source_conninfo = connections_config[project_config['source']]
     target_conninfo = connections_config[project_config['target']]
-    #print(source_conninfo)
-
-    source = utils.connection_from_config(source_conninfo)
-    target = utils.connection_from_config(target_conninfo)
 
     project = project_config.copy()
     del project['schemas']
-    #print(project_info)
 
     temp_path = project.get('temp_path')
     utils.create_path(temp_path)
-    #print(temp_path)
+
+    source_conninfos = []
+    target_conninfos = []
+    projects = []
+    schemas = []
+    tables = []
 
     for schema_config in project_config['schemas']:
         schema = schema_config.copy()
         del schema['tables']
-        #    print(schema)
-        #    print(schema_info)
         for table in schema_config['tables']:
-            run_load(source, target, project, schema, table)
-            #        print(schema['source_schema'])
-            #        print(schema['target_schema'])
-            #print(project)
-            #print(schema)
-            #print(table)
+            print("Starting loading a table")
+            source_conninfo_item = source_conninfo
+            target_conninfo_item = target_conninfo
+            project_item = project
+            schema_item = schema
+            table_item = table
+
+            source_conninfos.append(source_conninfo_item)
+            target_conninfos.append(target_conninfo_item)
+            projects.append(project_item)
+            schemas.append(schema_item)
+            tables.append(table_item)
+
+            #load = [source, target, project, schema, table]
+            #loads.append(load)
+
+            #run_load(source, target, project, schema, table)
+
+    #print(loads[''])
+
+    with Executor(max_workers=2) as executor:
+        for _ in executor.map(run_load, source_conninfos, target_conninfos, projects, schemas, tables):
+            pass
 
 
-def run_load(source, target, project, schema, table):
+def run_load(source_conninfo, target_conninfo, project, schema, table):
+    source = utils.connection_from_config(source_conninfo)
+    target = utils.connection_from_config(target_conninfo)
 
     # Temp_path
     temp_path = project.get('temp_path')
