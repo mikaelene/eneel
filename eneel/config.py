748c61d71a9b69426fa885951eb538d528bf855c
Mikael Ene <mikael.ene@gmail.com>
2020-02-04 22:16:29 +0100

    f stringified config.py

--- eneel/config.py
+++ eneel/config.py
@@ -23,7 +23,7 @@ def get_project(project):
     else:
         try:
             project_file_contents = utils.load_file_contents(
-                project + ".yml", strip=False
+                f"{project}.yml", strip=False
             )
             project = utils.load_yaml(project_file_contents)
 
